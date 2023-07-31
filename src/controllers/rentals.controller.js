c87b691c3917d0f560c8c4f52d0edec7cc07654a
ffukamizu <f.fukamizu@live.com>
2023-07-31 03:50:54 -0300

    feat: implemented rentals controller

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -1,9 +1,107 @@
 import { db } from '../database/database.connection.js';
+import dayjs from 'dayjs';
 
-export async function getRentals(req, res) {}
+export async function getRentals(req, res) {
+    try {
+        const rentals = await db.query(
+            `
+            SELECT rentals.*, 
+                JSON_BUILD_OBJECT("id", customers.id, "name", customers.name as customer, 
+                JSON_BUILD_OBJECT("id", games.id, "name", games.name) as game,
+                TO_CHAR(rentals."rentDate", 'YYYY-MM-DD') AS "rentDate",
+                TO_CHAR(rentals."returnDate", 'YYYY-MM-DD') AS "returnDate",
+            FROM rentals
+            JOIN customers ON rentals."customerId" = customers.id
+            JOIN games ON rentals."gameId" = games.id;
+            `
+        );
 
-export async function postRentals(req, res) {}
+        res.status(200).send(rentals.rows);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
 
-export async function postRentalsId(req, res) {}
+export async function postRentals(req, res) {
+    const { customerId, gameId, daysRented } = req.body;
 
-export async function deleteRentals(req ,res) {}
+    try {
+        const customers = await db.query(
+            `
+            SELECT *
+            FROM customers
+            WHERE id = $1;
+            `,
+            [customerId]
+        );
+
+        if (customers.rows.length === 0) return res.sendStatus(404);
+
+        const games = await db.query(
+            `
+            SELECT *
+            FROM games
+            WHERE id = $1;
+            `,
+            [gameId]
+        );
+
+        if (games.rows.length === 0) return res.sendStatus(404);
+
+        const rentals = await db.query(
+            `
+            SELECT COUNT(*)
+            FROM rentals
+            WHERE "gameId" = $1 AND "returnDate" IS null;
+            `,
+            [gameId]
+        );
+
+        if (Number(rented.rows[0].count) >= Number(game.rows[0].stockTotal)) {
+            return res.status(400).send('No copies available');
+        }
+
+        await db.query(
+            `
+            INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate, "originalPrice", "delayFee")
+            VALUES ($1, $2, $3, $4, $5, $6, $7);
+            `,
+            [customerId, gameId, dayjs().format('YYYY-MM-DD'), daysRented, null, daysRented * game.rows[0].pricePerDay, null]
+        );
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export async function postRentalsId(req, res) {
+    
+}
+
+export async function deleteRentals(req, res) {
+    const { id } = req.params;
+
+    try {
+        const rentals = await db.query(
+            `
+            SELECT *
+            FROM rentals
+            WHERE "id" = $1;
+            `,
+            [id]
+        );
+
+        if (rentals.rows.length === 0) return res.sendStatus(404);
+
+        if (rentals.rows[0].returnDate === null) return res.sendStatus(400);
+
+        await db.query(
+            `
+            DELETE FROM rentals
+            WHERE "id" = $1;
+            `,
+            [id]
+        );
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
