53d121a1fb646d46a5d726f5fc73bd046374490e
Fernando Fukamizu <128663623+ffukamizu@users.noreply.github.com>
2023-07-31 15:30:25 -0300

    Update database.connection.js

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -7,10 +7,9 @@ const { Pool } = pg;
 
 const configDatabase = {
   connectionString: process.env.DATABASE_URL,
+  ssl: false,
 };
 
 if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
 
 export const db = new Pool(configDatabase);
-
-export db;
