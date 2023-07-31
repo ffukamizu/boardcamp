e47a25087aa7801780766b9bd7ca9d83228386e8
ffukamizu <f.fukamizu@live.com>
2023-07-31 03:13:13 -0300

    feat: implemented customers controller

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -1,9 +1,95 @@
 import { db } from '../database/database.connection.js';
 
-export async function getCustomers(req, res) {}
+export async function getCustomers(req, res) {
+    try {
+        const customers = await db.query(
+            `
+            SELECT * 
+            FROM customers
+            `
+        );
 
-export async function getCustomersId(req, res) {}
+        res.status(200).send(customers.rows);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
 
-export async function postCustomers(req, res) {}
+export async function getCustomersId(req, res) {
+    const { id } = req.params;
 
-export async function putCustomers(req, res) {}
+    try {
+        const customers = await db.query(
+            `
+            SELECT *, TO_CHAR(birthday, 'YYYY-MM-DD') AS birthday 
+            FROM customers 
+            WHERE id = $1;
+            `,
+            [id]
+        );
+
+        if (customers.rows.length === 0) return res.sendStatus(404);
+
+        res.status(200).send(customers.rows[0]);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export async function postCustomers(req, res) {
+    const { name, phone, cpf, birthday } = req.body;
+
+    try {
+        const customers = await db.query(
+            `
+        SELECT *
+        FROM customers
+        WHERE cpf = $1
+        `,
+            [cpf]
+        );
+
+        if (customers.rows.length !== 0) return res.status(409).send('CPF already registered');
+
+        await db.query(
+            `
+        INSERT INTO customers ("name", "phone", "cpf", "birthday")
+        VALUES ($1, $2, $3, $4);
+        `,
+            [name, phone, cpf, birthday]
+        );
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export async function putCustomers(req, res) {
+    const { name, phone, cpf, birthday } = req.body;
+    const { id } = req.params;
+
+    try {
+        const customers = await db.query(
+            `
+        SELECT *
+        FROM customers
+        WHERE cpf = $1;
+        `,
+            [cpf]
+        );
+
+        if (customers.rows.length > 0 && customers.rows[0].id !== Number(id)) {
+            return res.status(409).send('CPF already registered');
+        }
+
+        await db.query(
+            `
+        UPDATE customers
+        SET "name" = $1, "phone" = $2, "cpf" = $3, "birthday" = $4
+        WHERE id = $5;
+        `,
+            [name, phone, cpf, birthday, id]
+        );
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -2,7 +2,12 @@ import { db } from '../database/database.connection.js';
 
 export async function getGames(req, res) {
     try {
-        const games = await db.query(`SELECT * FROM games`);
+        const games = await db.query(
+            `
+            SELECT * 
+            FROM games
+            `
+        );
 
         res.status(200).send(games.rows);
     } catch (err) {
@@ -13,9 +18,20 @@ export async function getGames(req, res) {
 export async function postGames(req, res) {
     const { name, image, stockTotal, pricePerDay } = req.body;
 
-    const games = await db.query(`SELECT * FROM games WHERE name = $1;`, [name]);
+    try {
+        const games = await db.query(
+            `
+        SELECT * 
+        FROM games 
+        WHERE name = $1;
+        `,
+            [name]
+        );
 
-    if (games.rows.length > 0) return res.status(400).send('Game already registered');
+        if (games.rows.length > 0) return res.status(400).send('Game already registered');
 
-    await db.query(`INSERT INTO games ("name", "image", "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay]);
+        await db.query(`INSERT INTO games ("name", "image", "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay]);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 }
