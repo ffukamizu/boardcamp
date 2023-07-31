eee3a222a5aad3cc364dcedbb2168fb6ea88f630
ffukamizu <f.fukamizu@live.com>
2023-07-31 02:49:18 -0300

    feat: implemented games controller

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,5 +1,21 @@
 import { db } from '../database/database.connection.js';
 
-export async function getGames(req, res) {}
+export async function getGames(req, res) {
+    try {
+        const games = await db.query(`SELECT * FROM games`);
 
-export async function postGames(req, res) {}
+        res.status(200).send(games.rows);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export async function postGames(req, res) {
+    const { name, image, stockTotal, pricePerDay } = req.body;
+
+    const games = await db.query(`SELECT * FROM games WHERE name = $1;`, [name]);
+
+    if (games.rows.length > 0) return res.status(400).send('Game already registered');
+
+    await db.query(`INSERT INTO games ("name", "image", "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay]);
+}
