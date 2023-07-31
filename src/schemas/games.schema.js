c266b3158f7c172025d4cabba45a7c83f3914c51
ffukamizu <f.fukamizu@live.com>
2023-07-31 17:33:59 -0300

    bug fixes

--- src/schemas/games.schema.js
+++ src/schemas/games.schema.js
@@ -1,10 +1,10 @@
 import Joi from 'joi';
 
 const schemaGames = Joi.object({
-    name: Joi.string().min(2).required(),
+    name: Joi.string().required(),
     image: Joi.required(),
-    stockTotal: Joi.number().min(2).required(),
-    pricePerDay: Joi.number().min(2).required(),
+    stockTotal: Joi.number().required(),
+    pricePerDay: Joi.number().required(),
 });
 
 export default schemaGames;
