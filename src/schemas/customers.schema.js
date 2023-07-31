13aff8eab3a33dfc5604b492207730146b5cb206
ffukamizu <f.fukamizu@live.com>
2023-07-31 17:22:43 -0300

    bug fixes

--- src/schemas/customers.schema.js
+++ src/schemas/customers.schema.js
@@ -1,8 +1,8 @@
 import Joi from 'joi';
 
 const schemaCustomers = Joi.object({
-    name: Joi.string().min(2).required(),
-    phone: Joi.number().min(2).required(),
+    name: Joi.string().required(),
+    phone: Joi.number().min(10).required(),
     cpf: Joi.string().pattern(/^\d{11}$/).required(),
     birthday: Joi.string().isoDate().required(),
 });
